195f1a57645cf4dcd264c3dd331f6a2b3718aa1d
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-05-07 16:04:23 -0700

    Update models.py

--- newapi/myproject/api/myapp/models.py
+++ newapi/myproject/api/myapp/models.py
@@ -47,6 +47,14 @@ class Order(models.Model):
         return f"Order {self.order_id} by {self.user.username}"
 
 class OrderItem(models.Model):
-    pass
+    #This will delete all object acosiated with the object order
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
     
+    @property
+    def  item_subtotal(self): 
+        return self.product.price * self.quantity
     
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order{self.order.order_id}"
