68dcd1b0e0b3227aa61eaabe3661c66e7245ebb3
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-05-08 13:16:34 -0700

    Update settings.py

--- newapi/myproject/api/api/settings.py
+++ newapi/myproject/api/api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
+    'django_extensions',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = "myapp.User"
+
+
