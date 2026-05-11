7c801370336ea2d7c527946eb804cf084102e241
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-05-11 00:19:57 -0700

    Update serializers.py

--- newapi/myproject/api/myapp/serializers.py
+++ newapi/myproject/api/myapp/serializers.py
@@ -17,8 +17,41 @@ class ProductSerializer(serializers.ModelSerializer):
                 "The price must be grated then 0."
             )
         return value 
-             
+
+#motification on 5/10   
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(max_digits=10,  decimal_places=2,source='product.price')
     
+    class Meta:
+        model = OrderItem
+        fields = ('product_name',
+                  'product_price',
+                  'quantity', 
+                  'item_subtotal')
+          
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+    
+    def get_total_price(slef, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items )
+    
+    class Meta:
+        model =  Order
+        fields =('order_id', 
+                 'created_at', 
+                 'user', 
+                 'status', 
+                 'items', 
+                 'total_price',)
+     
+
+
+
+
 
 
 
