249c975b3ecfc45a8c1ac6c9fd5101e62bddfbf1
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-05-12 22:40:48 -0700

    Update views.py

--- newapi/myproject/api/myapp/views.py
+++ newapi/myproject/api/myapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from myapp.serializers import ProductSerializer
-from myapp.models import Product
+from myapp.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
+from myapp.models import Product, Order
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from django.db.models import Max
 # Create your views here.
 
 
@@ -25,3 +25,13 @@ def order_list(request):
     serializer = OrderSerializer(order, many=True)
     return Response(serializer.data)
 
+
+@api_view()
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'products' : products,
+        'count' : len(products),
+        'max_price': products.aggregate(max_price=Max('price'))['max_price']  
+    })
+    return  Response(serializer.data)
