0b714437c471ae093db8d5835e0cf7f2454a65a5
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-05-27 13:37:34 -0700

    Update views.py

--- newapi/myproject/api/myapp/views.py
+++ newapi/myproject/api/myapp/views.py
@@ -1,30 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from myapp.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
-from myapp.models import Product, Order
+from myapp.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.db.models import Max
+from rest_framework import generics
+
 # Create your views here.
 
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
 
-@api_view(['GET'])
-def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
 
-@api_view(['GET'])
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
 
-@api_view(['GET'])
-def order_list(request):
-    order = Order.objects.prefetch_related('item_product')
-    serializer = OrderSerializer(order, many=True)
-    return Response(serializer.data)
 
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items__product')
+    serializer_class = OrderSerializer
+    
+ 
 
 @api_view()
 def product_info(request):
