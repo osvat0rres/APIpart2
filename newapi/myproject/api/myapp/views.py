b74a01cbad43cc78cc29aa77c3935fb394254944
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-04 14:12:35 -0700

    Update views.py

--- newapi/myproject/api/myapp/views.py
+++ newapi/myproject/api/myapp/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
 from myapp.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
 from myapp.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.db.models import Max
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
 
 # Create your views here.
 
@@ -33,8 +35,8 @@ class OrderListAPIView(generics.ListAPIView):
 class UserOrderListAPIView(generics.ListAPIView):
     queryset = Order.objects.prefetch_related('items__product')
     serializer_class = OrderSerializer
-    #only authenticated users can access their orders
-    permission_classes =[IsAuthenticated]
+    #Only authenticated users can access their orders
+    permission_classes = [IsAuthenticated]
     
     
         #dynamically filter orders
@@ -42,4 +44,15 @@ class UserOrderListAPIView(generics.ListAPIView):
         user = self.request.user
         qs = super().get_queryset()
         return qs.filter(user=user)
-
+    
+    
+class ProductInfoAPIView(APIView):
+    def get(self,request):
+        products = Product.objects.all()
+        serializer = ProductInfoSerializer({
+            'products' : products,
+            'count' : len(products),
+            'max_price': products.aggregate(max_price=Max('price'))['max_price']  
+        })
+        return  Response(serializer.data)
+        
