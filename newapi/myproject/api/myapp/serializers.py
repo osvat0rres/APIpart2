773c2058938566834ab0f67fa4ffc3b680ab0437
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-06 12:07:13 -0700

    Update serializers.py

--- newapi/myproject/api/myapp/serializers.py
+++ newapi/myproject/api/myapp/serializers.py
@@ -6,7 +6,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = (
-            'id',
+            'description',
             'name',
             'price',
             'stock',
