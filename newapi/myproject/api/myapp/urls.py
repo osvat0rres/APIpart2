370ec92ba2455ae661a6b9f78ed3191874a6ed5d
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-06 12:32:50 -0700

    Update urls.py

--- newapi/myproject/api/myapp/urls.py
+++ newapi/myproject/api/myapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.ProductListAPIView.as_view()),
-    path('products/create/', views.ProductCreateAPIView.as_view()),
+    path('products/', views.ProductListCreateAPIView.as_view()),
     path('product/info/', views.ProductInfoAPIView.as_view()),
     path('products/<int:pk>/', views.ProductDetailAPIView.as_view()),
     path('orders/', views.OrderListAPIView.as_view()),
     path('user-orders/', views.UserOrderListAPIView.as_view(), name='user-orders'),
+
 ]
