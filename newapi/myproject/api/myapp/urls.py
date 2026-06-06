82a5e6d3920087d9f5f5887ed209610da22c557b
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-06 12:06:29 -0700

    Update urls.py

--- newapi/myproject/api/myapp/urls.py
+++ newapi/myproject/api/myapp/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductListAPIView.as_view()),
+    path('products/create/', views.ProductCreateAPIView.as_view()),
     path('product/info/', views.ProductInfoAPIView.as_view()),
     path('products/<int:pk>/', views.ProductDetailAPIView.as_view()),
     path('orders/', views.OrderListAPIView.as_view()),
-    path('user-orders/', views.UserOrderListAPIView.as_view(), name='user-orders')
-    
+    path('user-orders/', views.UserOrderListAPIView.as_view(), name='user-orders'),
 ]
