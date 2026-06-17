1ed9cfed3a70042f082c5e276359e2c62bfb7405
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-16 23:19:15 -0700

    Update admin.py

--- newapi/myproject/api/myapp/admin.py
+++ newapi/myproject/api/myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderItem 
+from .models import Order, OrderItem, User
 # Register your models here.
 
 class OrderItemInline(admin.TabularInline):
@@ -11,3 +11,4 @@ class OrderAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Order, OrderAdmin)
+admin.site.register(User)
