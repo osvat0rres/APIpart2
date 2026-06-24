4e43dbc5bcff9f6dea9b612e785fb31616f98dcf
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-23 21:45:44 -0700

    Update views.py

--- newapi/myproject/api/myapp/views.py
+++ newapi/myproject/api/myapp/views.py
@@ -22,6 +22,10 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+      
+    #For the fields that you want to filter
+    filterset_class = ProductFilter
+
     def get_permissions(self):
         self.permission_classes = [AllowAny]
         if self.request.method == "POST":
