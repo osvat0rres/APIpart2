5cda990c4828080aebd7b89fb5c862e8c4119882
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-24 22:47:59 -0700

    Update filters.py

--- newapi/myproject/api/myapp/filters.py
+++ newapi/myproject/api/myapp/filters.py
@@ -1,11 +1,22 @@
 import django_filters
 from myapp.models import Product
+from rest_framework import filters
+
+
+#This will return only items that are in stock
+class InStockFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        #This will return only items that are in stock
+        return queryset.filter(stock__gt=0)
+        #This will return itmes that are not in stock
+        # return queryset.exclude(stock__gt=0)
+    
 
 
 class ProductFilter(django_filters.FilterSet):
     class Meta:
         model = Product
-        #The field product gives you control over how to look fgor the items
+        #The field product gives you control over how to look for the items
         fields = {
             'name' : ['exact', 'contains'],
             'price' : ['exact','lt', 'gt', 'range']
