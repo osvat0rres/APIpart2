fb99e985fe993652c8b24f3f75e32501ecb11bd1
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-06-25 23:03:58 -0700

    Implement pagination and update queryset ordering
    Added pagination settings to ProductListCreateAPIView and modified queryset ordering.

--- newapi/myproject/api/myapp/views.py
+++ newapi/myproject/api/myapp/views.py
@@ -13,6 +13,7 @@ from rest_framework.views import APIView
 from myapp.filters import ProductFilter, InStockFilterBackend
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework.pagination import PageNumberPagination ,LimitOffsetPagination 
 
 # Create your views here.
 
@@ -21,7 +22,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     #queryset = Product.objects.filter(stock__gt=0)
     #Producst that are out of stock
     #queryset = Product.objects.exclude(stock__gt=0)
-    queryset = Product.objects.all()
+    queryset = Product.objects.order_by('pk')
     serializer_class = ProductSerializer
     
     #For the fields that you want to filter (search)
@@ -36,6 +37,17 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     #If you want an exact match just add a '=' to the item ('=name')
     search_fields = ['name','description']
     ordering_filds = ['name','price','stock']
+    
+    #This will overwrite the default setting for pagination 
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 2
+    #This will give you the page number on the url
+    pagination_class.page_query_param = 'pagenum'
+    #The user will be abalable to select the number of pages it wants
+    pagination_class.page_size_query_param = 'size'
+    #set the max number of pages it wants
+    pagination_class.max_page_size = 3
+    
 
     def get_permissions(self):
         self.permission_classes = [AllowAny]
