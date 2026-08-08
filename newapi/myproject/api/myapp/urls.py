6f4d07e02ecec0ac2e4f4c63c6b43b0e25b1c9a1
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-08-07 22:55:55 -0600

    Add user list endpoint to URLs

--- newapi/myproject/api/myapp/urls.py
+++ newapi/myproject/api/myapp/urls.py
@@ -5,12 +5,13 @@ from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('products/', views.ProductListCreateAPIView.as_view()),
     path('product/info/', views.ProductInfoAPIView.as_view()),
-    path('products/<int:pk>/', views.ProductDetailAPIView.as_view()),
+    path('users/', views.UserListView.as_view()),
 
 ]
 
 router = DefaultRouter()
 router.register('orders', views.OrderViewSet)
 urlpatterns += router.urls
+
 
 
