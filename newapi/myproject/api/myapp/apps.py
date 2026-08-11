bae1faa1a8813cfa7f936da5af421833c90c3851
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-08-11 11:50:12 -0600

    Update apps.py

--- newapi/myproject/api/myapp/apps.py
+++ newapi/myproject/api/myapp/apps.py
@@ -2,4 +2,10 @@ from django.apps import AppConfig
 
 
 class MyappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
+    
+    def ready(self):
+        from . import signals
+
+ 
