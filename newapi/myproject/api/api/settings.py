433ccc050534a52817df5a091a66746ddf5a5d80
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-08-12 10:41:40 -0600

    Configure JWT token expiration settings
    Added configuration for JWT token lifetimes.

--- newapi/myproject/api/api/settings.py
+++ newapi/myproject/api/api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -158,4 +159,10 @@ CACHES = {
     }
 }
 
+#This code configures how long the JWT uthentication token stays valid 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME" : timedelta(minute= 60),
+    "REFRESH_TOKEN_LIFETIME" : timedelta(days=1),
+    
+}
 
