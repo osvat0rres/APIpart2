1470024060e3d19aff61c36a6ed6ebf95237adc9
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-08-16 12:30:49 -0700

    Update urls.py

--- newapi/myproject/api/myapp/urls.py
+++ newapi/myproject/api/myapp/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('products/', views.ProductListCreateAPIView.as_view()),
     path('product/info/', views.ProductInfoAPIView.as_view()),
+    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view(), name="product-detail"),
     path('users/', views.UserListView.as_view()),
 
 ]
@@ -12,6 +13,7 @@ urlpatterns = [
 router = DefaultRouter()
 router.register('orders', views.OrderViewSet)
 urlpatterns += router.urls
+
 
 
 
