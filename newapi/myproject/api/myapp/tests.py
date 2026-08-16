9dc6eb71b1e6b727f07d8792dae45f2bfae915e6
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-08-16 12:25:41 -0700

    Update tests.py

--- newapi/myproject/api/myapp/tests.py
+++ newapi/myproject/api/myapp/tests.py
@@ -1,30 +1,51 @@
 from django.test import TestCase
-from myapp.models import Order, User
+from myapp.models import Order, User, Product
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework import status
+
 
 # Create your tests here.
-class UserOrderTestCase(TestCase):
-    def setUp(self):
-        user1 = User.objects.create_user(username='user1', password='test')
-        user2 = User.objects.create_user(username='user2', password='test')
-        Order.objects.create(user=user1)
-        Order.objects.create(user=user1)
-        Order.objects.create(user=user2)
-        Order.objects.create(user=user2)
-        
-    def test_user_order_endpoint_retrieves_only_authenticated_user_orders(self):
-        user = User.objects.get(username='user2')
-        self.client.force_login(user)
-        response = self.client.get(reverse('user-orders'))
 
-        # You can also write a test with only the status number
-        assert response.status_code == status.HTTP_200_OK
-        orders = response.json()
-        self.assertTrue(all(order['user'] == user.id for order in orders))
+class ProductAPITestCase(APITestCase):
+    def setUp(self):
+        self.admin_user = User.objects.create_superuser(username="admin", password="adminpass")
+        self.normal_user = User.objects.create_user(username="user", password="userpass")
+        self.product = Product.objects.create(
+            description = "Test description",
+            name = "tes object",
+            price = 9.99,
+            stock = 10 
+        )
         
-    def test_user_order_list_unauthenticated(self):
-        response = self.client.get(reverse('user-orders'))
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.url = reverse('product-detail', kwargs={'product_id':self.product.pk})
+    
+    def test_get_product(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'],self.product.name)
+        
+    def test_unathorized_update_product(self):
+        data = {"name": "Updated Product"}
+        response = self.client.put(self.url, data)
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+        
+    def test_unathorized_delete_product(self):
+        response = self.client.put(self.url)
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+    
+    def test_only_admin_can_delete_Product(self):
+        self.client.login(username='user', password='userpass') 
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue(Product.objects.filter(pk=self.product.pk).exists())
         
     
+        self.client.login(username='admin', password='adminpass') 
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Product.objects.filter(pk=self.product.pk).exists())
+    
+        
+        
