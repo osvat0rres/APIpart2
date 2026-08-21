f2f6feca5db64df458ceecfd7f7b99bec51023b0
Osvaldo Torres <113087789+osvat0rres@users.noreply.github.com>
2026-08-21 14:42:35 -0700

    Add files via upload

---
+++ newapi/myproject/api/myapp/task.py
@@ -0,0 +1,11 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+@shared_task 
+def send_order_confirmation_emial(order_id, user_email):
+    subject = "Order confirmation"
+    message = f"Your order with  {order_id} has beed recive and is beend processed"
+    return send_mail(subject, message, settings.DEFAULT_FROM_EMAIL,[user_email]) 
+    
